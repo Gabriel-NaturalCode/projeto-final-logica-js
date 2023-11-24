481a4dc7e68a233159dba7ef3a44d2ef2b1051fa
Gabriel-NaturalCode <gabriel.viana2292@gmail.com>
2023-11-24 19:28:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-let listaDeTarefas = []
+let listaDeTarefas = [];
 
 
 // Função para adcionar tarefas à lista
 function lista() {
-    const tarefa = prompt("Adicione uma tarefa à sua lista")
+    const tarefa = prompt("Adicione uma tarefa à sua lista");
     if (tarefa != undefined){
-        listaDeTarefas.push(tarefa)
+        listaDeTarefas.push(tarefa);
     } else {
         return
     }
 while (confirm("Deseja adicionar outra?") == true) {
-    listaDeTarefas.push(prompt("Adicione a nova tarefa"))
+    listaDeTarefas.push(prompt("Adicione a nova tarefa"));
 }
 return listaDeTarefas
 }
 
 // Função para buscar tarefas através do ID
 function buscar() {
-    let idTarefa = prompt("Digite o ID da tarefa que você deseja buscar")
+    let idTarefa = prompt("Digite o ID da tarefa que você deseja buscar");
     if(idTarefa >= 0 && idTarefa < listaDeTarefas.length && idTarefa != undefined) {
-    alert("Tarefa solicitada: " +listaDeTarefas[idTarefa])
+    alert("Tarefa solicitada: " +listaDeTarefas[idTarefa]);
     } else {
-        alert("ID inválido")
+        alert("ID inválido");
     }
     
 }
 
 // Função para editar tarefa
 function editar() {
-    let itemEdit = prompt("Que tarefa você deseja editar?")
+    let itemEdit = prompt("Que tarefa você deseja editar?");
     if(itemEdit != undefined && itemEdit.length != 0) {
-        let listaEditada = listaDeTarefas.filter((item) => item != itemEdit)
-        let itemEditado = prompt("Qual o novo nome da tarefa?")
-        listaEditada.push(itemEditado)
+        let listaEditada = listaDeTarefas.filter((item) => item != itemEdit);
+        let itemEditado = prompt("Qual o novo nome da tarefa?");
+        listaEditada.push(itemEditado);
         return listaDeTarefas = listaEditada
     } else {
         return alert("Essa tarefa não consta na sua lista");
@@ -41,17 +41,17 @@ function editar() {
 
 // Função que remove tarefas da lista atavés da posição
 function remover() {
-    const itemParaRemover = prompt("Digite a posição da tarefa a ser excluída")
+    const itemParaRemover = prompt("Digite a posição da tarefa a ser excluída");
     if (itemParaRemover < listaDeTarefas.length && itemParaRemover >= 0 && itemParaRemover != undefined) {
-    listaDeTarefas.splice(itemParaRemover, 1)
-    alert("Ítem removido com sucesso")
+    listaDeTarefas.splice(itemParaRemover, 1);
+    alert("Ítem removido com sucesso");
     } else if (itemParaRemover){
-        alert("Essa posição não é válida")
+        alert("Essa posição não é válida");
     }
     return listaDeTarefas
 }
 
 // Função para mostrar lista de tarefas
 function listar() {
-    alert(listaDeTarefas)
+    alert(listaDeTarefas);
 }
